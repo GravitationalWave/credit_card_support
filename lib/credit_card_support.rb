9e5f456ef25fc09426078e44b73038b0c614205c
mxrguspxrt <margus@tione.eu>
2013-10-30 17:57:24 +0200

    If ActiveModel validations not loaded puts info about it

--- lib/credit_card_support.rb
+++ lib/credit_card_support.rb
@@ -1,3 +1,8 @@
 require 'credit_card_support/luhn_number'
 require 'credit_card_support/credit_card_number'
-require 'credit_card_support/validators/credit_card_number_validator' if Object.const_defined?(:ActiveModel)
+
+if Object.const_defined?(:ActiveModel)
+  require 'credit_card_support/validators/credit_card_number_validator'
+else
+  puts "CreditCardSupport: ActiveModel validations not loaded"
+end
--- lib/credit_card_support/version.rb
+++ lib/credit_card_support/version.rb
@@ -1,7 +1,7 @@
 module CreditCardSupport
   module Version
     MAJOR = 2
-    MINOR = 1
+    MINOR = 2
     PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
